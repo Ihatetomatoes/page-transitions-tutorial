b40c13bb9bcfb184f6a485f458d21d1bb07c5821
tomas938 <tomasalbert9387182@gmail.com>
2022-07-25 15:55:43 +0200

    barba js animation

--- js/main.js
+++ js/main.js
@@ -1,84 +1,80 @@
-function init(){
-    
-    const loader = document.querySelector('.loader');
+function init() {
+	const loader = document.querySelector(".loader");
 
-    // reset position of the loading screen
-    gsap.set(loader, {
-        scaleX: 0, 
-        rotation: 10, 
-        xPercent: -5,
-        yPercent: -50, 
-        transformOrigin: 'left center', 
-        autoAlpha: 1
-    });
+	// reset position of the loading screen
+	gsap.set(loader, {
+		scaleX: 0,
+		rotation: 10,
+		xPercent: -5,
+		yPercent: -50,
+		transformOrigin: "left center",
+		autoAlpha: 1,
+	});
 
-    function loaderIn() {
-        // GSAP tween to stretch the loading screen across the whole screen
-        return gsap.fromTo(loader, 
-            {
-                rotation: 10,
-                scaleX: 0,
-                xPercent: -5
-            },
-            { 
-                duration: 0.8,
-                xPercent: 0,
-                scaleX: 1, 
-                rotation: 0,
-                ease: 'Power4.inOut', 
-                transformOrigin: 'left center'
-            });
-    }
+	function loaderIn() {
+		// GSAP tween to stretch the loading screen across the whole screen
+		return gsap.fromTo(
+			loader,
+			{
+				rotation: 10,
+				scaleX: 0,
+				xPercent: -5,
+			},
+			{
+				duration: 0.8,
+				xPercent: 0,
+				scaleX: 1,
+				rotation: 0,
+				ease: "Power4.inOut",
+				transformOrigin: "left center",
+			}
+		);
+	}
 
-    function loaderAway() {
-        // GSAP tween to hide the loading screen
-        return gsap.to(loader, { 
-            duration: 0.8, 
-            scaleX: 0,
-            xPercent: 5, 
-            rotation: -10, 
-            transformOrigin: 'right center', 
-            ease: 'Power4.inOut'
-        });
-    }
+	function loaderAway() {
+		// GSAP tween to hide the loading screen
+		return gsap.to(loader, {
+			duration: 0.8,
+			scaleX: 0,
+			delay: 1,
+			xPercent: 5,
+			rotation: -10,
+			transformOrigin: "right center",
+			ease: "Power4.inOut",
+		});
+	}
 
-    // do something before the transition starts
-    barba.hooks.before(() => {
+	// do something before the transition starts
+	barba.hooks.before(() => {
+		document.querySelector("html").classList.add("is-transitioning");
+		barba.wrapper.classList.add("is-animating");
+	});
 
-        document.querySelector('html').classList.add('is-transitioning');
-        barba.wrapper.classList.add('is-animating');
+	// do something after the transition finishes
+	barba.hooks.after(() => {
+		document.querySelector("html").classList.remove("is-transitioning");
+		barba.wrapper.classList.remove("is-animating");
+	});
 
-    });
+	// scroll to the top of the page
+	barba.hooks.enter(() => {
+		window.scrollTo(0, 0);
+	});
 
-    // do something after the transition finishes
-    barba.hooks.after(() => {
-
-        document.querySelector('html').classList.remove('is-transitioning');
-        barba.wrapper.classList.remove('is-animating');
-
-    });
-
-    // scroll to the top of the page
-    barba.hooks.enter(() => {
-
-        window.scrollTo(0, 0);
-
-    });
-
-    barba.init({
-        transitions: [{
-            async leave() {
-                await loaderIn();
-        
-            },
-            enter() {
-                loaderAway();
-            }
-        }]
-    })
-
+	barba.init({
+		transitions: [
+			{
+				async leave() {
+					await loaderIn();
+				},
+				enter() {
+					loaderAway();
+				},
+			},
+		],
+	});
 }
 
-window.addEventListener('load', function(){
-    init();
+window.addEventListener("load", function () {
+	init();
 });
